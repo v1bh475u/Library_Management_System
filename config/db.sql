d433d0e5cbb31b66a7cb4275e05d87274c09e2ac
v1bh475u <maulikbarot2915@gmail.com>
2024-06-05 22:43:10 +0530

    Database Changes

--- config/db.sql
+++ config/db.sql
@@ -2,18 +2,20 @@ CREATE DATABASE IF NOT EXISTS `users_db`;
 
 CREATE TABLE `users`(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL SECONDARY KEY,
     `password` VARCHAR(255) NOT NULL,
-    `role` VARCHAR(255) NOT NULL
+    `role` VARCHAR(255) NOT NULL,
+    `messages` INT
 );
 
 CREATE TABLE `requests`(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL,
-    `book_id` INT,
+    `book_id` INT FOREIGN KEY REFERENCES books(id),
     `title` VARCHAR(255) NULL,
-    `request` VARCHAR(255) NOT NULL,
+    `request` ENUM('pending','approved','disapproved') NOT NULL,
     `status` VARCHAR(255) NOT NULL,
+    `user_status` ENUM('seen','unseen','pending') NOT NULL,
     `date` DATE NOT NULL
 );
 
@@ -23,14 +25,14 @@ CREATE TABLE `books`(
     `author` VARCHAR(255) NOT NULL,
     `genre` VARCHAR(255) NOT NULL,
     `status` VARCHAR(255) NOT NULL,
-    `borrower` VARCHAR(255)
+    `quantity` INT NOT NULL
 );
 
 CREATE TABLE `borrowing_history`(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `book_id` INT,
+    `book_id` INT FOREIGN KEY REFERENCES books(id),
     `title` VARCHAR(255) NOT NULL,
-    `borrower` VARCHAR(255) NOT NULL,
+    `borrower` VARCHAR(255) NOT NULL FOREIGN KEY REFERENCES users(username),
     `borrowed_date` DATE NOT NULL,
     `returned_date` DATE
 );
